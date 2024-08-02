fc27e26caabaae46dee28a69adb016df2cbe1877
Erick Aguilar Gomez <65264358+ErickAguilarGomez@users.noreply.github.com>
2024-08-02 21:14:56 +0000

    terminado

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  //El evento de carga ya esta añadido de manera inicial asi que no es necesario utilizarlo
+  const numbers = [
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K",
+    "A"
+  ];
+  const symbols = [
+    "fas fa-heart",
+    "fas fa-diamond",
+    "fas fa-club",
+    "fas fa-spade"
+  ];
+
+  const icons = document.querySelectorAll(".icons");
+  const numberToShow = document.querySelector(".numberToShow");
+  const Nramdon = Math.random();
+  icons.forEach(i => {
+    i.className += `${symbols[Math.floor(Nramdon * symbols.length)]} fa-4x`;
+  });
+  numberToShow.textContent = numbers[Math.floor(Nramdon * numbers.length)];
+  console.log(icons[0]);
 };
